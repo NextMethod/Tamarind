a4eb510ebd2598e9f8f6fb894a319fdbc45a1e89
Jordan S. Jones <Jordan.Jones@Software.Dell.com>
2013-09-12 10:08:46 -0600

    Refactored to be interfaces.
    Also, using the built it IReadOnlyDictionary instead of
    ImmutableDictionary to reduce the number of dependencies.

---
+++ Tamarind/Cache/ICacheLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tamarind.Cache
+{
+	public interface ICacheLoader<TKey, TValue>
+	{
+
+		TValue Load(TKey key);
+
+		IReadOnlyDictionary<TKey, TValue> LoadAll(IEnumerable<TKey> keys);
+
+		Task<TValue> Reload(TKey key, TValue oldValue);
+
+	}
+}
