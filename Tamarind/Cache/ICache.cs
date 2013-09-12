10a10007567c1e523e6c874b4f07b9ff5cf09b70
earaya <esteban.araya@gmail.com>
2013-09-11 20:23:15 -0600

    Add link to Guava implementation of Cache.

--- Tamarind/Cache/ICache.cs
+++ Tamarind/Cache/ICache.cs
@@ -5,6 +5,7 @@ using System.Collections.Immutable;
 
 namespace Tamarind.Cache
 {
+    // Guava Reference: https://code.google.com/p/guava-libraries/source/browse/guava/src/com/google/common/cache/Cache.java
     // TODO: NEEDS DOCUMENTATION.
     public interface ICache<K, V>
     {
