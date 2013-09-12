089d592baa2579e53db2aefb19d1cc0dbac30e54
earaya <esteban.araya@gmail.com>
2013-09-11 20:31:17 -0600

    Added ICache interface documentation.

--- Tamarind/Cache/ICache.cs
+++ Tamarind/Cache/ICache.cs
@@ -6,7 +6,16 @@ using System.Collections.Immutable;
 namespace Tamarind.Cache
 {
     // Guava Reference: https://code.google.com/p/guava-libraries/source/browse/guava/src/com/google/common/cache/Cache.java
-    // TODO: NEEDS DOCUMENTATION.
+    /// <summary>
+    /// A semi-persistent mapping from keys to values. Cache entries are manually added using
+    /// <see cref="Get"/> or <see cref="Put"/>, and are stored in the cache until
+    /// either evicted or manually invalidated.
+    /// 
+    /// Implementations of this interface are expected to be thread-safe, and can be safely accessed
+    /// by multiple concurrent threads.
+    /// </summary>
+    /// <typeparam name="K">The cache's key type.</typeparam>
+    /// <typeparam name="V">The cache's value type.</typeparam>
     public interface ICache<K, V>
     {
         V GetIfPresent(K key);
