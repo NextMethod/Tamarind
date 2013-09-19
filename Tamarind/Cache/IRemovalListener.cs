45808e6983fe8c685ccec2b9cec37a30c19e4f12
earaya <esteban.araya@gmail.com>
2013-09-18 18:48:41 -0600

    Add IRemovalListener interface.

---
+++ Tamarind/Cache/IRemovalListener.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamarind.Cache
+{
+    public interface IRemovalListener<TKey, TValue>
+    {
+        void onRemoval(RemovalNotification<TKey, TValue> notification);
+    }
+}
