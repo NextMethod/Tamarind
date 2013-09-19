72e31e9f12ac688f2f1a8db6e33bffddad5d96ce
earaya <esteban.araya@gmail.com>
2013-09-18 18:57:36 -0600

    Documentation for IRemovalListener.

--- Tamarind/Cache/IRemovalListener.cs
+++ Tamarind/Cache/IRemovalListener.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace Tamarind.Cache
 {
+    /// <summary>
+    ///     An object that can receive a notification when an entry is removed from a cache. The removal
+    ///     resulting in notification could have occured to an entry being manually removed or replaced, or
+    ///     due to eviction resulting from timed expiration, exceeding a maximum size, or garbage
+    ///     collection.
+    ///     <para>
+    ///         This class holds strong references to the key and value, regardless of the type of references the cache may
+    ///         be using.
+    ///     </para>
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
     public interface IRemovalListener<TKey, TValue>
     {
+        /// <summary>
+        /// Notifies the listener that a removel has occured (at some point in the past).
+        /// </summary>
+        /// <param name="notification">The cache item removal notification.</param>
         void onRemoval(RemovalNotification<TKey, TValue> notification);
     }
 }
