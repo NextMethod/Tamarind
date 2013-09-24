e7c148391efe9ae515c1e554f0a208831ae37ac5
earaya <esteban.araya@gmail.com>
2013-09-23 21:39:56 -0600

    Add IStatsCounter interface.

---
+++ Tamarind/Cache/IStatsCounter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamarind.Cache
+{
+    /// <summary>
+    /// Accumulates statistics during the operation of a <see cref="Cache"/> for presentation by 
+    /// <see cref="IStatsCounter"/> . This is solely intended for consumption by <see cref="Cache"/> implementors.
+    /// </summary>
+    public interface IStatsCounter
+    {
+        void RecordHits(int count);
+        void RecordMisses(int count);
+        void RecordLoadSuccess(long loadTime);
+        void RecordLoadException(long loadTime);
+        void RecordEviction();
+        ICacheStats Snapshot();
+    }
+}
