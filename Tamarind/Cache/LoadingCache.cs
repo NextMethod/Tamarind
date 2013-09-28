a47fb59d3c9d5a1f24951f82bf6fd800d6570bf7
earaya <esteban.araya@gmail.com>
2013-09-27 23:32:07 -0600

    LoadingCache docs.

--- Tamarind/Cache/LoadingCache.cs
+++ Tamarind/Cache/LoadingCache.cs
@@ -7,6 +7,21 @@ using System.Threading.Tasks;
 
 namespace Tamarind.Cache
 {
+    /// <summary>
+    /// This class provides a skeletal implementation of the {@code Cache} interface to minimize the
+    /// effort required to implement this interface.
+    ///     <para>
+    ///         To implement a cache, the programmer needs only to extend this class and provide an
+    ///         implementation for the <see cref="Get"/> and <see cref="getIfPresent" /> methods.
+    ///         <see cref="Get(TKey, Func<TValue>)"/>, and <see cref="GetAll"/> are implemented in
+    ///         terms of <see cref="Get"/>; <see cref="GetAllPresent"/> is implemented in terms of <see cref="GetIfPresent"/>;
+    ///         <see cref="PutAll"/> is implemented in terms of <see cref="Put"/>, <see cref="InvalidateAll(IEnumerable)"/> is
+    ///         implemented in terms of <see cref="Invalidate"/>. The method <see cref="CleanUp"/> is a no-op. All other
+    ///         methods throw an <see cref="NotImplementedException"/>.
+    ///     </para>
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
     public abstract class LoadingCache<TKey, TValue> : Cache<TKey, TValue>, ILoadingCache<TKey, TValue>
     {
         public TValue Get(TKey key)
